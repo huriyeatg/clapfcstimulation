b84722b8fe07845a5cb8a2b562f0f1ee6f722530
Huriye Atilgan <huriye.atilgan@gmail.com>
2023-09-19 14:09:51 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # clapfcstimulation
+
+Problematic recordings - excluded from start : PAQI/O not started so that no stimEvent times
+2021-05-04	21101	5	157.85	-678.23	124.03	{'block'},{'PAQ '},10;	1	0	0	1	1 Packi/o not clicked
+2022-04-07	22101	1	-323	-110.05	249.98	block2 (PAQ + pupil)	1	1	1	0	0 weird PackI/O problem)
+
+2022-04-08	22102	8	-746.52	-721.11	206.8	block2 (PAQ + pupil)	0	1	1	0	0	  Packi/o not clicked
+2022-04-12	22107	7	-496.73	-1455.72	229.75	block2 (PAQ + pupil)	1	1	0	0	0 Packi/o not clicked
+2022-04-12	22107	8	-177.54	-3557.91	99.93	block2 (PAQ + pupil)	1	1	0	0	0  weird PackI/O problem)
+2022-04-17	22102	13	-351.11	-538.65	150.28	{'block2'}, {'PAQ + pupil'};	1	1	0	1	0 ( weird PackI/O problem)
+2022-04-18	22107	6	-428.76	-112.59	126.62	{'block2'}, {'PAQ + pupil'};	1	1	0	0	0 ( weird PackI/O problem)
+2022-04-20	22108	11	-179.76	-435.11	137.57	{'block2'}, {'PAQ + pupil'};	1	1	0	1	0 ( weird PackI/O problem)
+2022-04-20	22105	2	512.92	-1233.75	149.8	{'block2'}, {'PAQ + pupil'};	1	1	0	0 ( weird PackI/O problem)
+
+There is one extra weird TTL pulse
+2022-04-16	22101	10	-334.43	-720.32	212.87	{'block2'}, {'PAQ + pupil'};	1	1	0	0	0
+2022-04-17	22102	2	-169.28	-250.75	184.15	{'block2'}, {'PAQ + pupil'};	1	1	1	1	0
+
+
+
+
--- run_DLCforPupil.py
+++ run_DLCforPupil.py
@@ -1,35 +1,19 @@
-# Pupil preprocessing - uses Deep Lab Cut a custom model for pupil extraction
-
+import main_funcs as mfun
 import deeplabcut
 import glob
+info = mfun.analysis()
 
-# create the configpath
-configPath = "C:\\Users\\Huriye\\Documents\\code\\DeepLabCut-master\\pupilExtraction\\Updated 5 Dot Training Model-Eren CAN-2021-11-21\\"
-path_config_file = configPath + "config.yaml" # Enter the path of the config file that was just created from the above step (check the folder)
-print(path_config_file)
+# Get the config file - the model trained by Eren.
+path_config_file = info.DLCconfigPath
 
-
-# get the list of videos
-for folder in fList:
-    folderDateName = (folder[0:10])
-   # print(folderDateName)
-    fpath     = os.path.join(input_path,str(folderDateName))
-    fpath     = fpath.replace(os.sep, '/')
-    fpath     = Path(fpath)
-   # print(fpath)
-    save_path  = os.path.join(output_path,folder)
-    save_path  = save_path.replace(os.sep, '/')
-   # print(save_path)
-videofile_path = ['D:\DLC_output'] #Enter a folder OR a list of videos to analyze.
-deeplabcut.analyze_videos(path_config_file,videofile_path, videotype='.avi',save_as_csv=True)
-
-# new way:
-configfile, path_train_config = deeplabcut.create_pretrained_project(
-    Task,
-    YourName,
-    video,
-    model=MODEL_NAME,
-    videotype="avi",
-    analyzevideo=True,
-    createlabeledvideo=True,
-    copy_videos=False,
+# Create csv files
+for ind, recordingDate in enumerate(info.recordingList.recordingDate):
+    if info.recordingList.pupilImaging[ind] ==1: # if pupil recording is done & converted to .avi file
+        try:
+            filepathname = (info.recordingList.filepathname[ind] +'_p-' + 
+                            format(int(info.recordingList.recordingID[ind]), '03d') )
+            videofilename  = [f for f in glob.glob(filepathname + "\\*.avi")]
+            print('Pupil size extraction from video:' + filepathname)
+            deeplabcut.analyze_videos(path_config_file, videofilename, videotype='.avi',save_as_csv=True)
+        except:
+            print(str(ind) + ': Failed')
